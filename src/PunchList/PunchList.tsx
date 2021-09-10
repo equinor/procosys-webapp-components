f374bc64c4c4605f02bfcf0b56715d257e254e78
VRiveland <vriv@equinor.com>
2021-09-10 12:32:46 +0200

    Added PunchList component

---
+++ src/PunchList/PunchList.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { ScopeWrapper } from '../Scope/Scope';
+import { useState } from 'react';
+import { AsyncStatus, PunchPreview } from '../services/apiTypes';
+import { useHistory } from 'react-router';
+import AsyncPage from '../components/AsyncPage';
+import Filter from '../components/Filter/Filter';
+import InfoItem from '../InfoItem/InfoItem';
+import removeSubdirectories from '../utils/removeSubdirectories';
+
+type PunchListProps = {
+    url: string;
+    fetchPunchListStatus: AsyncStatus;
+    punchList?: PunchPreview[];
+};
+
+const PunchList = ({
+    url,
+    punchList,
+    fetchPunchListStatus,
+}: PunchListProps): JSX.Element => {
+    const history = useHistory();
+    const [filteredPunchList, setFilteredPunchList] = useState<
+        PunchPreview[] | undefined
+    >(punchList);
+
+    return (
+        <ScopeWrapper>
+            <AsyncPage
+                fetchStatus={fetchPunchListStatus}
+                errorMessage={
+                    'Error: Unable to get punch list. Please try again.'
+                }
+                emptyContentMessage={'The punch list is empty.'}
+            >
+                <>
+                    <Filter
+                        url={url}
+                        setShownPunches={setFilteredPunchList}
+                        punchItems={punchList}
+                    />
+                    {filteredPunchList?.map((punch) => (
+                        <InfoItem
+                            key={punch.id}
+                            status={punch.status}
+                            statusLetters={[
+                                punch.cleared ? 'C' : null,
+                                punch.verified ? 'V' : null,
+                            ]}
+                            attachments={punch.attachmentCount}
+                            headerText={punch.id.toString()}
+                            description={punch.description}
+                            chips={[punch.formularType, punch.responsibleCode]}
+                            tag={punch.tagNo}
+                            onClick={(): void =>
+                                history.push(
+                                    `${removeSubdirectories(url)}/punch-item/${
+                                        punch.id
+                                    }`
+                                )
+                            }
+                        />
+                    ))}
+                </>
+            </AsyncPage>
+        </ScopeWrapper>
+    );
+};
+
+export default PunchList;
