846ec4740e7e797d65736be02699d404e8ddddbb
VRiveland <vriv@equinor.com>
2021-09-10 12:29:23 +0200

    Added Scope component

---
+++ src/Scope/Scope.tsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router';
+import styled from 'styled-components';
+import AsyncPage from '../components/AsyncPage';
+import Filter from '../components/Filter/Filter';
+import InfoItem from '../InfoItem/InfoItem';
+import { AsyncStatus, ChecklistPreview } from '../services/apiTypes';
+
+export const ScopeWrapper = styled.div`
+    & h3 {
+        text-align: center;
+        margin-top: 16px;
+    }
+`;
+
+type ScopeProps = {
+    url: string;
+    fetchScopeStatus: AsyncStatus;
+    scope?: ChecklistPreview[];
+};
+
+const Scope = ({ url, fetchScopeStatus, scope }: ScopeProps): JSX.Element => {
+    const history = useHistory();
+    const [shownScope, setShownScope] = useState<
+        ChecklistPreview[] | undefined
+    >(scope);
+
+    return (
+        <ScopeWrapper>
+            <AsyncPage
+                errorMessage={'Unable to load scope. Please try again.'}
+                emptyContentMessage={'The scope is empty.'}
+                fetchStatus={fetchScopeStatus}
+            >
+                <div>
+                    <Filter
+                        url={url}
+                        setShownScope={setShownScope}
+                        scopeItems={scope}
+                    />
+                    {shownScope?.map((checklist) => (
+                        <InfoItem
+                            isScope
+                            key={checklist.id}
+                            attachments={checklist.attachmentCount}
+                            status={checklist.status}
+                            statusLetters={[
+                                checklist.isSigned ? 'S' : null,
+                                checklist.isVerified ? 'V' : null,
+                            ]}
+                            headerText={checklist.tagNo.toString()}
+                            description={checklist.tagDescription}
+                            chips={[
+                                checklist.formularType,
+                                checklist.responsibleCode,
+                            ]}
+                            onClick={(): void =>
+                                history.push(`${url}/checklist/${checklist.id}`)
+                            }
+                        />
+                    ))}
+                </div>
+            </AsyncPage>
+        </ScopeWrapper>
+    );
+};
+
+export default Scope;
