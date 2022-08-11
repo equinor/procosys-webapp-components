3fa1af98277e8d97ccc488a7a7de51b0266cf569
VRiveland <vriv@equinor.com>
2022-08-11 13:32:16 +0200

    Added bookmark button

--- src/components/EntityDetails/EntityDetails.tsx
+++ src/components/EntityDetails/EntityDetails.tsx
@@ -1,7 +1,8 @@
-import { Typography } from '@equinor/eds-core-react';
+import { Button, Typography } from '@equinor/eds-core-react';
 import React from 'react';
 import styled from 'styled-components';
 import { Caption, COLORS } from '../../style/GlobalStyles';
+import EdsIcon from '../icons/EdsIcon';
 
 const EntityDetailsWrapper = styled.article<{ isDetailsCard?: boolean }>`
     cursor: ${(props): string => (props.isDetailsCard ? 'default' : 'pointer')};
@@ -46,6 +47,11 @@ const HeaderWrapper = styled.div<{ isDetailsCard?: boolean }>`
     }
 `;
 
+const BookmarkWrapper = styled.div`
+    margin-left: 80px;
+    margin-top: -15px;
+`;
+
 type EntityDetailsProps = {
     isDetailsCard?: boolean;
     icon: JSX.Element;
@@ -53,6 +59,9 @@ type EntityDetailsProps = {
     description?: string;
     details?: string[];
     onClick?: () => void;
+    isBookmarked?: boolean;
+    offlinePlanningState?: boolean;
+    handleBookmarkClicked?: () => Promise<void>;
 };
 
 const EntityDetails = ({
@@ -62,6 +71,9 @@ const EntityDetails = ({
     description,
     details,
     onClick,
+    isBookmarked,
+    offlinePlanningState,
+    handleBookmarkClicked,
 }: EntityDetailsProps): JSX.Element => {
     return (
         <EntityDetailsWrapper
@@ -81,6 +93,26 @@ const EntityDetails = ({
                     {description}
                 </Typography>
             </ContentWrapper>
+            {offlinePlanningState && handleBookmarkClicked && (
+                <BookmarkWrapper>
+                    <Button
+                        variant="ghost_icon"
+                        onClick={(e: React.MouseEvent<HTMLElement>): void => {
+                            e.stopPropagation();
+                            handleBookmarkClicked();
+                        }}
+                    >
+                        <EdsIcon
+                            color={COLORS.mossGreen}
+                            name={
+                                isBookmarked
+                                    ? 'bookmark_filled'
+                                    : 'bookmark_outlined'
+                            }
+                        />
+                    </Button>
+                </BookmarkWrapper>
+            )}
         </EntityDetailsWrapper>
     );
 };
