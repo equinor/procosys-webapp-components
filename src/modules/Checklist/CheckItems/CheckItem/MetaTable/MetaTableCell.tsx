8c5aea67ad0b47186eee1683c01cf1085e6ae871
Vilde Riveland <vriv@equinor.com>
2023-02-07 12:02:16 +0100

    Bug/99652 date field in checklist not saved (#70)
    * Fixed metatable cell
    
    * Prettify
    
    * Cleanup

--- src/modules/Checklist/CheckItems/CheckItem/MetaTable/MetaTableCell.tsx
+++ src/modules/Checklist/CheckItems/CheckItem/MetaTable/MetaTableCell.tsx
@@ -124,26 +124,16 @@ const MetaTableCell = ({
                         id={dateId}
                         role="datepicker"
                         value={inputValueDate}
-                        onFocus={(
-                            e: React.FocusEvent<
-                                HTMLTextAreaElement | HTMLInputElement
-                            >
-                        ): void => {
-                            setValueBeforeFocus(e.target.value);
-                        }}
-                        onBlur={(
-                            e: React.FocusEvent<
-                                HTMLTextAreaElement | HTMLInputElement
-                            >
-                        ): void => {
-                            e.target.value !== valueBeforeFocus &&
-                                submitData(putDateCellApiCall);
+                        onBlur={(): void => {
+                            submitData(putDateCellApiCall);
                         }}
                         onChange={(
                             event: React.ChangeEvent<
                                 HTMLTextAreaElement | HTMLInputElement
                             >
-                        ): void => setInputValueDate(event.target.value)}
+                        ): void => {
+                            setInputValueDate(event.target.value);
+                        }}
                     />
                 </MetaTableDateCellWrapper>
             ) : (
